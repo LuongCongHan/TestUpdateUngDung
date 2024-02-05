f793aaa5ad0fd0e08648e107b6971b0635e73a3e
Luong Cong Han <luongconghan.ute@gmail.com>
2024-02-05 22:31:41 +0700

    add update.json

---
+++ LamViecVoiFile/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LamViecVoiFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDirection = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtV1 = new System.Windows.Forms.TextBox();
+            this.txtV2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDirection
+            // 
+            this.btnDirection.Location = new System.Drawing.Point(38, 50);
+            this.btnDirection.Name = "btnDirection";
+            this.btnDirection.Size = new System.Drawing.Size(88, 39);
+            this.btnDirection.TabIndex = 0;
+            this.btnDirection.Text = "Direction";
+            this.btnDirection.UseVisualStyleBackColor = true;
+            this.btnDirection.Click += new System.EventHandler(this.btnDirection_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Assembly";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtV1
+            // 
+            this.txtV1.Location = new System.Drawing.Point(263, 32);
+            this.txtV1.Name = "txtV1";
+            this.txtV1.Size = new System.Drawing.Size(100, 20);
+            this.txtV1.TabIndex = 3;
+            this.txtV1.Text = "1.23.56.1487";
+            // 
+            // txtV2
+            // 
+            this.txtV2.Location = new System.Drawing.Point(263, 69);
+            this.txtV2.Name = "txtV2";
+            this.txtV2.Size = new System.Drawing.Size(100, 20);
+            this.txtV2.TabIndex = 3;
+            this.txtV2.Text = "1.24.55.487";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Version 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Version 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 214);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtV2);
+            this.Controls.Add(this.txtV1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDirection);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDirection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtV1;
+        private System.Windows.Forms.TextBox txtV2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LamViecVoiFile/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LamViecVoiFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDirection_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@"C:\Users\Dell Latitude 3540\Desktop\File cai dat CapNhatUngDung InstallForge\Thumuc4");
+            Directory.Delete(@"C:\Users\Dell Latitude 3540\Desktop\File cai dat CapNhatUngDung InstallForge\Thumuc3",true);
+            var files= Directory.GetDirectories(@"C:\Users\Dell Latitude 3540\Desktop\File cai dat CapNhatUngDung InstallForge");
+            string txt = null;
+            foreach (var item in files)
+            {
+                txt += item + Environment.NewLine;
+            }
+            label1.Text = txt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string v1 = "1.23.56.1487";
+            string v2 = "1.24.55.487";
+
+            string txt = "";
+            var version1 = new Version(txtV1.Text);
+            var version2 = new Version(txtV2.Text);
+
+            var result = version1.CompareTo(version2);
+            if (result > 0)
+                txt="version1 is greater";
+            else if (result < 0)
+                txt="version2 is greater";
+            else
+                txt="versions are equal";
+
+            label1.Text = txt;
+
+            //var ass = Assembly.GetExecutingAssembly();
+            //label1.Text = ass.Location;
+            //Process.Start(ass.Location);
+        }
+        
+
+    }
+}
